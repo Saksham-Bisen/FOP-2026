4b2ea6a19e47045099b14a7f8f063c58b7f87e52
Saksham-Bisen <sakshambisen_2318@outlook.com>
2026-01-30 11:51:39 +0000

    Figuring out how to use github

---
+++ Test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+int a ;
+
+printf("Hello World !\n");
+printf("Enter First number:");
+scanf("%d", &a);
+printf("You entered:%d", a);
+
+
+
+
+
+
+
+    return 0;
+}
