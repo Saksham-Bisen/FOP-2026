da6c551495bfcadfd638427d35123f4089ce8f7a
Saksham-Bisen <sakshambisen_2318@outlook.com>
2026-01-31 06:12:12 +0000

    New Changes

--- Switches.c
+++ Switches.c
@@ -4,36 +4,35 @@
 
 int main(){
 
-
 int choice;
-int num1, num2;
+float num1, num2;
 
 printf("Enter the first number:");
-scanf("%d", &num1);
+scanf("%f", &num1);
 
 printf("Enter the second number:");
-scanf("%d", &num2);
+scanf("%f", &num2);
 
-printf("Enter the desired number\n 1 for addition\n2 for subtraction\n3 for multiplication\n4 for subtraction\n");
+printf("Enter the desired number\n 1 for addition\n2 for subtraction\n3 for multiplication\n4 for Division\n");
 scanf("%d", &choice);
 
 switch (choice)
 {
 case(1):
-    printf("The addition of two numbers is :%d", num1+num2);
-    break;
+    printf("The addition of two numbers is :%.2f", num1+num2);
+break;
 case(2):
-    printf("The difference of two numbers is :%d", num1-num2);
-    break;
+    printf("The difference of two numbers is :%.2f", num1-num2);
+break;
 case(3):
-    printf("The Multiplication of two numbers is :%d", num1*num2);
-    break;
+    printf("The Multiplication of two numbers is :%.2f", num1*num2);
+break;
 case(4):
-    printf("The Quotient after Division of two numbers is :%d", num1/num2);
-    break;
+    printf("The Result after Division of two numbers is :%.2f", num1/num2);
+break;
 default:
     printf("Invalid number entered");
-    break;
+break;
 }
 
     return 0;
