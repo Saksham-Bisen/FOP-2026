b725b035cfde781bfdaa58e3dd78df9ab491e0a2
Saksham-Bisen <sakshambisen_2318@outlook.com>
2026-01-31 02:10:08 +0000

    new changes

--- Printing table of a number(Assignment-).c
+++ Printing table of a number(Assignment-).c
@@ -4,9 +4,7 @@ int main(){
 
 int i;
 int a ;
-
-printf("Hello World !\n");
-printf("Enter First number:");
+printf("Enter a number:");
 scanf("%d", &a);
 
 printf("The table of %d is:\n", a);
@@ -14,9 +12,5 @@ printf("The table of %d is:\n", a);
 for(i=1;i<=10;i++){
     printf("%d X %d = %d\n", a, i, a*i);
 }
-
-
-
-
     return 0;
 }
