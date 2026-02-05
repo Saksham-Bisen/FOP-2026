62f920d1689a3e776f27859b0dc155851e282e47
Saksham-Bisen <sakshambisen_2318@outlook.com>
2026-02-05 06:13:04 +0000

    removed math library

--- Assignment-02.c
+++ Assignment-02.c
@@ -13,7 +13,7 @@ scanf("%f", &num1);
 printf("Enter the second number:");
 scanf("%f", &num2);
 
-printf("Enter the desired number\n 1 for addition\n2 for subtraction\n3 for multiplication\n4 for Division\n5 for square\n6 for factorial");
+printf("Enter the desired number\n 1 for addition\n2 for subtraction\n3 for multiplication\n4 for Division\n5 for square\n6 for factorial\n");
 scanf("%d", &choice);
 
 switch (choice)
@@ -34,14 +34,14 @@ case(4):
     else{printf("The Result after Division of two numbers is :%.2f", num1/num2);}
 break;
 case(5):
-    printf("The Square of two numbers are :%d, %d", pow(num1,2), pow(num2,2));
+    printf("The Square of two numbers are :%.2f, %.2f", num1*num1, num2*num2);
 break;
 case(6):
-    printf("The Factorial of two numbers is :%.2f", num1*num2);
-    int fac1=1;
-    int fac2=1;
-    int count1 = num1;
-    int count2 = num2;
+
+    float fac1=1;
+    float fac2=1;
+    float count1 = num1;
+    float count2 = num2;
 
     while(count1>=1){
         fac1 = fac1*count1;
@@ -52,7 +52,12 @@ case(6):
         fac2 = fac2*count2;
         count2--;
     }
-printf("the factorial of %d is %d\nthe factorial of %d is %d", num1, fac1, num2, fac2);
+
+    if(num1 == 0)
+        fac1=0;
+    if(num2==0)
+        fac2=0;
+printf("the factorial of %f is %.2f\nthe factorial of %f is %.2f", num1, fac1, num2, fac2);
 break;
 default:
     printf("Invalid number entered");
