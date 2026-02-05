622d5856e12310d43153a85f250c8368adc39f84
Saksham-Bisen <sakshambisen_2318@outlook.com>
2026-02-05 05:59:35 +0000

    new changes

--- Assignment-02.c
+++ Assignment-02.c
@@ -12,7 +12,7 @@ scanf("%f", &num1);
 printf("Enter the second number:");
 scanf("%f", &num2);
 
-printf("Enter the desired number\n 1 for addition\n2 for subtraction\n3 for multiplication\n4 for Division\n");
+printf("Enter the desired number\n 1 for addition\n2 for subtraction\n3 for multiplication\n4 for Division\n5 for square\n6 for factorial");
 scanf("%d", &choice);
 
 switch (choice)
@@ -31,6 +31,12 @@ case(4):
         printf("Cannot divide by zero");
     }
     else{printf("The Result after Division of two numbers is :%.2f", num1/num2);}
+break;
+case(5):
+    printf("The Square of two numbers are :%.2f", num1*num2);
+break;
+case(6):
+    printf("The Multiplication of two numbers is :%.2f", num1*num2);
 break;
 default:
     printf("Invalid number entered");
