f7104f0b63d737252aa3cc59a8324c09150c1d0c
Saksham-Bisen <sakshambisen_2318@outlook.com>
2026-02-06 06:02:03 +0000

    optimised the code

--- Reversing_number.c
+++ Reversing_number.c
@@ -3,17 +3,14 @@
 int main(){
 
 int num;
-int copy;
 
 printf("Enter a number:");
 scanf("%d", &num);
 
-copy = num;
-
-while(copy!=0){
-
-printf("%d", copy%10);
-copy = copy/10;
+while(num!=0){
+printf("%d", num%10);
+num /= 10;
 }
+
     return 0;
 }
