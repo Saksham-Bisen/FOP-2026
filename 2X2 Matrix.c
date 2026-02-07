234e0c58024bf9cedde6ee9be0d2c1fb932ca950
Saksham-Bisen <sakshambisen_2318@outlook.com>
2026-02-07 09:40:50 +0000

    Fixed the code

--- 2X2 Matrix.c
+++ 2X2 Matrix.c
@@ -7,33 +7,21 @@ int mat[2][2];
 int row ;
 int col ;
 
-printf("Enter elements Column Wise\n");
+printf("Enter elements Row Wise\n");
 
 for(row=0;row<=1;row++){
-
     for(col=0;col<=1;){
         scanf("%d", &mat[row][col]);
-        col++;
-    }
+        col++;}
 }
 
 printf("Printing the elements of the matrix\n");
 
-
-printf("%d\n", mat[0][0]);
-printf("%d\n", mat[0][1]);
-printf("%d\n", mat[1][0]);
-printf("%d\n", mat[1][1]);
-
 for(row=0;row<=1;row++){
-   
     for(col=0;col<=1;){
-        printf("%d\n1", mat[row][col]);
-        printf("%d\n", row);
-        printf("%d\n", col);
-        col++;
-        
-    }
+        printf("%d\t", mat[row][col]);
+        col++;}
+    printf("\n");
 }
     return 0;
 }
