c3aff3e8c89136e35b9d726d1cfe4e9919cb9ccd
Saksham-Bisen <sakshambisen_2318@outlook.com>
2026-02-12 05:48:07 +0000

    Assignment-12

--- Greatest common divisor.c
+++ Greatest common divisor.c
@@ -4,12 +4,21 @@ int main(){
 
 int num1;
 int num2;
+
+int old_div=1;
+int new_div; 
+
 printf("Enter the first number:");
 scanf("%d", &num1);
 
 printf("Enter the second number:");
 scanf("%d", &num2);
 
-
+for(old_div;old_div<num1 && old_div<num2;old_div++){
+    if(num1%old_div==0 && num2%old_div==0){
+        new_div = old_div;
+    }
+}
+printf("The GCD of numbers is %d", new_div);
     return 0;
 }
