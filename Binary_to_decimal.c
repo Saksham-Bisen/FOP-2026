26e0590dc62e58e6290b4ca94958fde260483321
Saksham-Bisen <sakshambisen_2318@outlook.com>
2026-03-20 05:56:32 +0000

    ass-8,14

---
+++ Binary_to_decimal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+int num;
+int pwr = 0;
+int sum;
+
+printf("Enter a Binary Number:");
+scanf("%d", &num);
+
+for(num;num > 0;num/=10){
+sum += (num%10)*pow(2,num);
+}
+
+printf("The number in decimal is:%d", sum);
+    return 0;
+}
