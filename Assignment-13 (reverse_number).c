3bffa856aa4c204b2af1c4186d4e3a818fb448c8
Saksham-Bisen <sakshambisen_2318@outlook.com>
2026-03-20 18:13:37 +0000

    NC

--- Assignment-13 (reverse_number).c
+++ Assignment-13 (reverse_number).c
@@ -4,13 +4,12 @@ int main(){
 
 int num;
 
-
 printf("Enter a number:");
 scanf("%d", &num);
 
-
-for(num;num>0;num/=10){
+while(num!=0){
     printf("%d", num%10);
+    num/=10;
 }
     return 0;
 }
