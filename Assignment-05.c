3c1300f04dc9a7760afef605fc66547b592427fc
Saksham-Bisen <sakshambisen_2318@outlook.com>
2026-04-07 13:53:25 +0000

    ABC

--- Assignment-05.c
+++ Assignment-05.c
@@ -17,7 +17,6 @@ int mat1[n_row][n_col];
 int mat2[n_row][n_col];
 
 int R1 = 0;
-
 printf("Enter Matrix-1 elements row-wise:");
 while(R1<n_row){
     int C1 = 0;
@@ -39,26 +38,86 @@ while(R2<n_row){
 R2++;
 }
 
-
+printf("First Matrix:\n");
 int R3 = 0;
 while(R3<n_row){
     int C3 = 0;
     while(C3<n_col){
-        printf("|%d|", mat1[R3][C3] + mat2[R3][C3]);
+        printf("|%d|", mat1[R3][C3]);
         C3++;
     }
     printf("\n");
 R3++;
 }
 
+printf("Second Matrix:\n");
+int R4 = 0;
+while(R4<n_row){
+    int C4 = 0;
+    while(C4<n_col){
+        printf("|%d|", mat2[R4][C4]);
+        C4++;
+    }
+    printf("\n");
+R4++;
+}
+printf("Addition of two matrices:\n");
+int R5 = 0;
+while(R5<n_row){
+    int C5 = 0;
+    while(C5<n_col){
+        printf("|%d|", mat1[R5][C5] + mat2[R5][C5]);
+        C5++;
+    }
+    printf("\n");
+R5++;
+}
 
 
+/*int sad = 0;
+int R6 = 0;
 
 
+while(R6<n_row){
+    int in_r = 0;
+    int in_c = 0;
+    int C6 = 0;
+    int R6t  = 0;
 
 
+    while(C6<n_col){
+            
+            int C6t = 0;
 
+        sad = mat1[R6t][C6t];
+        in_c = C6t + 1;
+        while(in_c < n_col){
+            if(sad < mat1[R6t][C6t]){
+                C6t++;
+                in_c++;
+            }
+            else{sad = mat1[R6t][C6t];
+                C6t++;
+            in_c++;}
+            }
+            in_r = R6t + 1;
+            while(in_r < n_row){
+            if(sad > mat1[in_r][in_c]){
+                in_r++;
+            }
+            else{break;}
+        }
+        
+        C6++ ;
 
+    }
+R6++;
+}
+
+//printf("\n %d %d",R6t,  in_c);
+printf("\n Saddle pint is %d", sad);
+
+*/
 
 
 
