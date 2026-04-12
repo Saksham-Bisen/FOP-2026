53f9c3c682658dc0a1635e3f2ed97e2271ee2e83
Saksham-Bisen <sakshambisen_2318@outlook.com>
2026-04-12 09:22:47 +0000

    Implemented substring checking

--- Assignment-18.c
+++ Assignment-18.c
@@ -4,7 +4,8 @@
 
 int main(){
 
-char string[20];
+char string1[20];
+char string2[20];
 int lngt; //variable for string length
 int lp_cnt; //variable for loop counter for reversing
 int pal_cnt; //variable for palindrome loop
@@ -12,10 +13,13 @@ int i = 0; //variable 2 for palindrome
 bool pal = true; //initial value of palindrome variable set to true
 
 printf("Enter a string:");
-scanf("%s", string);
+scanf("%s", string1);
+
+printf("Enter a second string for substring checking:");
+scanf("%s", string2);
 
 // Finding the string length
-lngt = strlen(string);
+lngt = strlen(string1);
 printf("The length of string is %d\n", lngt);
 
 
@@ -25,14 +29,14 @@ printf("The string printed in reverse is:");
 lp_cnt = lngt - 1;
 
 for(lp_cnt;lp_cnt>=0;lp_cnt--){
-    printf("%c", string[lp_cnt]);}
+    printf("%c", string1[lp_cnt]);}
 
 
 
 // Checking whether the string is a palindrome or not
 pal_cnt = lngt - 1;
 while(pal_cnt>=0){
-    if(string[i] == string[pal_cnt]){
+    if(string1[i] == string1[pal_cnt]){
         pal = true;
         i++;
         pal_cnt--;
@@ -44,5 +48,14 @@ if(pal){
     printf("\nThe string is a palindrome");}
 else{printf("\nThe string is not a palindrome");}
 
+
+// Checking for sustring
+printf("\n");
+if(strstr(string1, string2)!= NULL){
+    printf("The substring is present in the main string");
+}
+else{
+    printf("The substring is not present in the main string");
+}
     return 0;
 }
